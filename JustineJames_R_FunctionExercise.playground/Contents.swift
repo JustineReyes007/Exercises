d8caaaf6aa617a340306945a2383882f1a7b34c2
JustineReyes007 <67969003+JustineReyes007@users.noreply.github.com>
2020-07-27 11:47:51 -0500

    Add files via upload

---
+++ JustineJames_R_FunctionExercise.playground/Contents.swift
@@ -0,0 +1,33 @@
+//first try
+func Addition () {
+    let a = 9
+    let b = 100
+    let c = a + b
+    print(c)
+}
+Addition()
+
+
+
+//second try
+func addtwo (a: Int, b: Int) -> Int {
+    var sum = 0
+    sum = a / b
+    
+    return sum
+}
+var Sumtotal = addtwo(a: 1400, b: 3)
+
+
+
+
+//third try
+var a = 10
+var b = 40
+
+func plus(min: Int, max: Int) -> Int {
+    let a = 10
+    let b = 40
+    return a + b
+}
+print(plus(min: a, max: b))
